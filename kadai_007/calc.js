ad930f33e4984bef9180c92dbcd2f8fbbc5076cb
yuya3145 <yuyayamada3145@gmail.com>
2025-02-05 21:54:47 +0900

    kadai_007

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let num = Math.floor(Math.random() * 16);
+
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
+}
+
+
+else if (num % 5 == 0){
+  console.log('5の倍数です');
+}
+else if (num % 3 == 0){
+  console.log('3の倍数です');
+}
+else {
+console.log(num);
+}
+
